a749333638f6d7e448c2d410612178d14cf316a9
Korotkov Dmitry <2110821@mail.ru>
2021-12-25 16:05:34 +0300

    Reports

--- src/test/java/API/DataForAPI.java
+++ src/test/java/API/DataForAPI.java
@@ -15,11 +15,15 @@ public class DataForAPI {
         String cvc;
     }
 
-    public static BodyForApi getBodyForApiWithValidCard () {
-        return new BodyForApi(DataGenerator.validCardNumber(),DataGenerator.generateValidYear(),DataGenerator.generateValidMonth(),DataGenerator.generateFullNameInEng(),DataGenerator.generateCVC());
+    public static BodyForApi getBodyForApiWithValidCard() {
+        return new BodyForApi(DataGenerator.validCardNumber(), DataGenerator.generateValidYear(), DataGenerator.generateValidMonth(), DataGenerator.generateFullNameInEng(), DataGenerator.generateCVC());
     }
 
-    public static BodyForApi getBodyForApiWithDeclinedCard () {
-        return new BodyForApi(DataGenerator.declinedCardNumber(),DataGenerator.generateValidYear(),DataGenerator.generateValidMonth(),DataGenerator.generateFullNameInEng(),DataGenerator.generateCVC());
+    public static BodyForApi getBodyForApiWithDeclinedCard() {
+        return new BodyForApi(DataGenerator.declinedCardNumber(), DataGenerator.generateValidYear(), DataGenerator.generateValidMonth(), DataGenerator.generateFullNameInEng(), DataGenerator.generateCVC());
+    }
+
+    public static BodyForApi getInvalidBody () {
+        return new BodyForApi("1111222233334444","39","99","467548","fgh");
     }
 }
